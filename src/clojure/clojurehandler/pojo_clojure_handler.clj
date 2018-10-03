6603fe2712249d4cb4f9cdd362530db2d62fc3fd
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2018-10-03 16:34:01 +0100

    fixed method name

--- src/clojure/clojurehandler/pojo_clojure_handler.clj
+++ src/clojure/clojurehandler/pojo_clojure_handler.clj
@@ -5,5 +5,5 @@
   :name "clojurehandler.POJOClojureHandler"
   :methods [[myhandler [javahandler.Request com.amazonaws.services.lambda.runtime.Context] javahandler.Response]])
 
-(defn -handleRequest [this request ctx]
+(defn -myhandler [this request ctx]
   (Response. (str "Hello " (.getFirstName request) " " (.getLastName request) "!")))
